18ffb5f61ccc2a7275c0c02b91256671e09bf7e4
CharlieRojas <crc_185@yahoo.com>
2022-05-18 14:42:28 -0500

    commit tree b50e8f7ed1a3ce0feabab854d08fd44bf9d1ad67

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction","Non-Fiction"]}
+
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+        if title && !(self.title.include?("Won't Believe") || self.title.include?("Secret")  || self.title.include?("Top [number]") || self.title.include?("Guess"))
+            errors.add(:title, "this is not clickbait!")
+        end
+    end
+    # def title_is_clickbait
+    #     self.title.include?("Won't Believe") || self.title.include?("Secret")  || self.title.include?("Top [number]") || self.title.include?("Guess")
+    #         errors.add("not clickbait-y")
+    #     end
+    # end
 end
